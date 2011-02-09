9e4e479701157db57a797ebe214372b546a4b0cf
ronaldwan <ronald__wan@hotmail.com>
2011-02-09 18:32:16 -0500

    adding exchange access token helper method

--- lib/foursquare_node.rb
+++ lib/foursquare_node.rb
@@ -1,6 +1,5 @@
 module Foursquare
   class Node
-
     def initialize(access_token)
       @access_token       = access_token
       @base_url = "https://api.foursquare.com:443/v2/"
@@ -10,7 +9,7 @@ module Foursquare
       require 'net/http'
 
       @query_string = "?"
-      @query_string += "oauth_token=#{CGI.escape(@access_token)}" unless @access_token.blank?
+      @query_string += "oauth_token=#{CGI.escape(@access_token)}" unless @access_token.empty?
 
       if method=="get"
         params.each{|key, val| @query_string += "&#{key}=#{val}"}
@@ -24,8 +23,21 @@ module Foursquare
 
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      response = JSON.parse(http.start {|http| http.request(request) }.body)
+      response = JSON.parse(http.start {|http| http.request(request)}.body)
       response
     end
+  end
+  
+  def self.exchange_access_token(code, client_id, client_secret, callback)
+    require 'net/http'
+
+    callback = CGI.escape(callback)
+
+    url = URI.parse("https://foursquare.com:443/oauth2/access_token?client_id=#{client_id}&client_secret=#{client_secret}&grant_type=authorization_code&redirect_uri=#{callback}&code=#{code}")
+    request = Net::HTTP::Get.new("#{url.path}?#{url.query}",{"Content-Type"=>"text/json"})
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    response = JSON.parse(http.start {|http| http.request(request)}.body)
+    response['access_token']
   end
 end
